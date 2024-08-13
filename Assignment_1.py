c804963ece39f209595a8f2672c224819a1472f1
Anurag <anuragcpyadav031@gmail.com>
2024-08-13 10:19:30 +0530

    Assignment_1

---
+++ Assignment_1.py
@@ -0,0 +1,48 @@
+# ---------------------- Perform operations on datatypes --------------------- #
+
+# 1. Identify DataTypes
+
+Student_name = "John"
+Student_age = 21
+Student_weight_in_kgs = 71.4
+Student = True
+
+print(type(Student_name))
+print(type(Student_age))
+print(type(Student_weight_in_kgs))
+print(type(Student))
+
+# 2. Arithmatic Operataions
+
+num_1 = 24
+num_2 = 26
+sum = num_1 + num_2
+print(num_1, "+", num_2, "=", sum)
+
+num_1 = 46
+num_2 = 28
+sum = num_1 - num_2
+print(num_1, "-", num_2, "=", sum)
+
+num_1 = 4
+num_2 = 21
+sum = num_1 * num_2
+print(num_1, "x", num_2, "=", sum)
+
+num_1 = 4
+num_2 = 2
+sum = num_1 / num_2
+print(num_1, "/", num_2, "=", sum)
+
+# 3. Concatenation
+
+str_1 = "Hello"
+str_2 = "Python"
+final_str = str_1 + " " + str_2
+print(final_str)
+
+num_1 = 20
+num_2 = 24
+final_str = str(num_1) + str(num_2)
+print(final_str)
+
